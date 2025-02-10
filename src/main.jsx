5f2b8a352e246447bf5ecd53e055eb76b61d1ea4
KishDevXR <kishorekish1503@gmail.com>
2025-02-10 20:05:50 +0530

    shareable link

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import PreviewViewer from './PreviewViewer.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import routing components
+import "./index.css";
+import App from "./App.jsx";
+import SharePage from "./SharePage.jsx"; // Import the SharePage component
 
-
-createRoot(document.getElementById('root')).render(
+// Render the app with routing
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        {/* Default route for the main app */}
+        <Route path="/" element={<App />} />
+
+        {/* Route for the shareable page */}
+        <Route path="/share/:shareId" element={<SharePage />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
