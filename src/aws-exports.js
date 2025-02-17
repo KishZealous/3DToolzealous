9da1e9a66656b0536745f98501300c7d13165c84
KishDevXR <kishorekish1503@gmail.com>
2025-02-17 16:33:52 +0530

    update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsconfig = {
     },
     Storage: {
       AWSS3: {
-        bucket: 'zspace-demo', // Your S3 bucket name
+        bucket: 'z-storage', // Your S3 bucket name
         region: 'ap-south-1',
       }
     }
